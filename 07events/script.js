c749a5c65732a75226c78b57b73030f5a977dd99
Eriy Suratman <eriy5819@gmail.com>
2022-04-29 07:02:34 +0700

    master

--- 07events/script.js
+++ 07events/script.js
@@ -8,15 +8,15 @@
 // const p2 = document.querySelector(".p2");
 // p2.onclick = ubahWarnaP2;
 
-// eventsListener
-// const p4 = document.querySelector("section#b p");
-// p4.addEventListener("click", function () {
-//   const ul = document.querySelector("section#b ul");
-//   const liBaru = document.createElement("li");
-//   const teksLiBaru = document.createTextNode("item baru");
-//   liBaru.appendChild(teksLiBaru);
-//   ul.appendChild(liBaru);
-// });
+// eventsListener;
+const p4 = document.querySelector("section#b p");
+p4.addEventListener("click", function () {
+  const ul = document.querySelector("section#b ul");
+  const liBaru = document.createElement("li");
+  const teksLiBaru = document.createTextNode("item baru");
+  liBaru.appendChild(teksLiBaru);
+  ul.appendChild(liBaru);
+});
 
 // const p3 = document.querySelector(".p3");
 // p3.onclick = function () {
